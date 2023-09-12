0ad1fb7091ae73e5942ce26a9d91048f2e1c3315
Danarquio <danarquio@outlook.com>
2023-09-11 23:14:04 -0300

    primera preentrega

---
+++ src/controllers/CartManager.js
@@ -0,0 +1,93 @@
+import {promises as fs} from "fs"
+import { nanoid } from "nanoid"
+import ProductManager from "./ProductManager.js"
+
+const productAll = new ProductManager
+
+class CartManager {
+    constructor() {
+        this.path = "./src/models/carts.json"
+        this.nextId = null;
+        this.initNextId()
+    }
+
+    async initNextId() {
+        this.nextId = await this.calculateNextId();
+    }
+
+    calculateNextId = async () => {
+        try {
+            let carts = await this.readCarts();
+            if (carts.length === 0) {
+                // Si no hay carritos, comienza con 1
+                return 1;
+            } else {
+                // Encuentra el ID más alto en los carritos existentes y suma 1
+                const maxId = Math.max(...carts.map((cart) => parseInt(cart.id)));
+                return maxId + 1;
+            }
+        } catch (error) {
+            console.error("Error al calcular el próximo ID:", error);
+            return null;
+        }
+    }
+
+    readCarts = async () => {
+        let carts = await fs.readFile(this.path , "utf-8")
+        return JSON.parse(carts)
+    }
+
+    writeCarts = async (carts) =>{
+        await fs.writeFile(this.path , JSON.stringify(carts))
+        
+    }
+
+    exist = async(id) => {
+        let carts= await this.readCarts()
+        return carts.find(carts => carts.id === id)
+    }
+
+    addCarts = async () => {
+        let cartsOld = await this.readCarts();
+        let id = this.nextId.toString(); // Usa el ID actual y conviértelo a cadena
+        this.nextId++; // Incrementa el ID en 1 para la próxima vez
+        let cartsConcat = [{ id: id, products: [] }, ...cartsOld];
+        await this.writeCarts(cartsConcat);
+        return "Carrito Agregado";
+    }
+
+    getCartsById = async (id) => {
+        let cartById = await this.exist(id)
+        if ( !cartById) return "Carrito no encontrado :("
+        return cartById
+    }
+
+    addProductInCart = async (cartId, productId) => {
+        let cartById = await this.exist(cartId)
+        if ( !cartById) return "Carrito no encontrado :("
+        let productById= await productAll.exist(productId)
+        if ( !productById) return "Producto no encontrado :("   
+        
+        let cartsAll =await this.readCarts ()
+        let cartFilter = cartsAll.filter(cart=> cart.id != cartId) 
+
+        if(cartById.products.some((prod)=> prod.id === productId)){
+            let moreproductInCar = cartById.products.find((prod)=> prod.id === productId)
+            moreproductInCar.cantidad++
+
+            let cartsConcat = [cartById, ...cartFilter]
+            await this.writeCarts(cartsConcat)
+            return "Producto sumado al Carrito"
+        }
+
+        cartById.products.push({id : productById.id, cantidad : 1})
+        let cartsConcat = [cartById, ...cartFilter]        
+        await this.writeCarts(cartsConcat)
+        return "Producto Agregado al Carrito"
+    }
+
+}
+
+
+
+export default CartManager
